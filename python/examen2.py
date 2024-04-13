e268418abade57c267708c8d23e18ddcb1b0bf64
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-04-13 11:25:23 -0600

    examen2: seguismo

--- python/examen2.py
+++ python/examen2.py
@@ -8,6 +8,7 @@
 Examen II
 """
 
+from random import randint
 import alumnos
 from ecuaciones_lineales import problema_de_ecuaciones_lineales
 
@@ -17,7 +18,14 @@ def main():
     Comentario de la función
     """
     lista = alumnos.leer_alumnos("alumnos.csv")
-    problema_de_ecuaciones_lineales()
+    tam = len(lista)
+
+    for i in range(tam):
+        siguiente = randint(0,len(lista)-1)
+        alumno = lista[siguiente]
+        lista.pop(siguiente)
+        print("Por favor\n\n", alumno["nombre"], alumno["apellidos"], "\n\npasa al pizarrón y ")
+        problema_de_ecuaciones_lineales()
 
 
 if __name__ == "__main__":
